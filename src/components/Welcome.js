6c044de827221436d56abebd719fa859bab935f7
Rohan Gupta <rohangupta788@gmail.com>
2022-02-13 23:20:55 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default Welcome=(props)=>{
+function Welcome(props){
     return(<>
 <h1>Hey {props.name}!</h1>
 <h2>Welcome to Newton School.</h2>
 </>
     );
 }
+export default Welcome;
