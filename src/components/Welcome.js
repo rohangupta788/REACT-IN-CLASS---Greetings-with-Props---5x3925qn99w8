cfb76a60e023f3d382690de8b051785016d551fd
Rohan Gupta <rohangupta788@gmail.com>
2022-02-14 12:20:23 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,20 @@
 import React from "react";
-
-function Welcome(props){
-    let sayhi=`hey ${props.name}!`;
-    let greetmessage= `Welcome to Newton School.`
-    return(
+import '../styles/App.css';
+import App from "./App";
+const Welcome = ({name}) => {
+    let sayHi = `Hey ${name}!`;
+    let greetMessage = `Welcome to Newton School.`;
+    return (
         <div>
-<h1>{sayhi}</h1>
-<h2>{greetmessage}</h2>
-
-  </div>  );
+            <h1>{sayHi}</h1>
+            <h2>{greetMessage}</h2>
+        </div>
+    );
 }
-
-
-
+const functionExample=()=>
+{
+    return (
+    <Welcome/>
+    );
+}
 export default Welcome;
